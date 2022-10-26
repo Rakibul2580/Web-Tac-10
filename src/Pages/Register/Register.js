92090694a3d9680a1f10f14bfef7ec829efb48a4
Rakibul2580 <rh565153@gmail.com>
2022-10-26 21:37:38 +0300

    register update

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import RegisterImg from "./11067-registration-animation.gif";
 
 const Register = () => {
   const { signUp, signInWithGoogle, signInWithGitHub, updatePro } =
@@ -32,155 +33,119 @@ const Register = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <section className="py-6 dark:bg-gray-800 dark:text-white">
-      <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+    <section className="py-6 dark:bg-white dark:text-gray-800">
+      <div className="md:flex justify-around">
         <div className="py-6 md:py-0 md:px-6">
-          <h1 className="text-4xl font-bold">Get in touch</h1>
-          <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
-          <div className="space-y-4">
-            <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span>Fake address, 9999 City</span>
-            </p>
-            <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
-              </svg>
-              <span>123456789</span>
-            </p>
-            <p className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-              </svg>
-              <span>contact@business.com</span>
-            </p>
-          </div>
+          <h1 className="text-4xl font-bold text-center my-3">
+            Please Register
+          </h1>
+          <img src={RegisterImg} alt="" className="w-96 md:w-full" />
         </div>
-        <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
-          <h1 className="text-2xl font-bold text-center">Login</h1>
-          <form
-            onSubmit={handleLogin}
-            className="space-y-6 ng-untouched ng-pristine ng-valid"
-          >
-            <div className="space-y-1 text-sm">
-              <label htmlFor="username" className="block dark:text-gray-400">
-                Username
-              </label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Username"
-                className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-              />
+        <div className="mx-10 md:mx-0 h-full">
+          <div className="w-full  p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
+            <h1 className="text-2xl font-bold text-center">Register</h1>
+            <form
+              onSubmit={handleLogin}
+              className="space-y-6 ng-untouched ng-pristine ng-valid text-black"
+            >
+              <div className="space-y-1 text-sm">
+                <label htmlFor="username" className="block dark:text-gray-400">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                  className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+                />
+              </div>
+              <div className="space-y-1 text-sm">
+                <label htmlFor="username" className="block dark:text-gray-400">
+                  Photo URL
+                </label>
+                <input
+                  type="text"
+                  name="photoUrl"
+                  id="photoUrl"
+                  placeholder="photo URL"
+                  className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+                />
+              </div>
+              <div className="space-y-1 text-sm">
+                <label htmlFor="password" className="block dark:text-gray-400">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  placeholder="email"
+                  className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+                />
+              </div>
+              <div className="space-y-1 text-sm">
+                <label htmlFor="password" className="block dark:text-gray-400">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  placeholder="Password"
+                  className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+                />
+              </div>
+              <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">
+                Sign up
+              </button>
+            </form>
+            <div className="flex items-center pt-4 space-x-1">
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+              <p className="px-3 text-sm dark:text-gray-400">
+                Register with social accounts
+              </p>
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
             </div>
-            <div className="space-y-1 text-sm">
-              <label htmlFor="username" className="block dark:text-gray-400">
-                Photo URL
-              </label>
-              <input
-                type="text"
-                name="photoUrl"
-                id="photoUrl"
-                placeholder="photourl"
-                className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-              />
+            <div className="flex justify-center space-x-4">
+              <button
+                onClick={handleGoogle}
+                aria-label="Log in with Google"
+                className="p-3 rounded-sm"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  className="w-5 h-5 fill-current"
+                >
+                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                </svg>
+              </button>
+              <button
+                onClick={handleGitHub}
+                aria-label="Log in with GitHub"
+                className="p-3 rounded-sm"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  className="w-5 h-5 fill-current"
+                >
+                  <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                </svg>
+              </button>
             </div>
-            <div className="space-y-1 text-sm">
-              <label htmlFor="password" className="block dark:text-gray-400">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                required
-                placeholder="email"
-                className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-              />
-            </div>
-            <div className="space-y-1 text-sm">
-              <label htmlFor="password" className="block dark:text-gray-400">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                required
-                placeholder="Password"
-                className="border w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-              />
-            </div>
-            <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">
-              Sign in
-            </button>
-          </form>
-          <div className="flex items-center pt-4 space-x-1">
-            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-            <p className="px-3 text-sm dark:text-gray-400">
-              Register with social accounts
+            <p className="text-xs text-center sm:px-6 dark:text-gray-400">
+              have an account?
+              <Link
+                rel="noopener noreferrer"
+                to="/login"
+                className="underline dark:text-gray-100"
+              >
+                Log In
+              </Link>
             </p>
-            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
           </div>
-          <div className="flex justify-center space-x-4">
-            <button
-              onClick={handleGoogle}
-              aria-label="Log in with Google"
-              className="p-3 rounded-sm"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                className="w-5 h-5 fill-current"
-              >
-                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-              </svg>
-            </button>
-            <button
-              onClick={handleGitHub}
-              aria-label="Log in with GitHub"
-              className="p-3 rounded-sm"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                className="w-5 h-5 fill-current"
-              >
-                <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-              </svg>
-            </button>
-          </div>
-          <p className="text-xs text-center sm:px-6 dark:text-gray-400">
-            have an account?
-            <Link
-              rel="noopener noreferrer"
-              to="/login"
-              className="underline dark:text-gray-100"
-            >
-              Log In
-            </Link>
-          </p>
         </div>
       </div>
     </section>
